c853692a05755d1181d94674f986e1ccb0085d79
Adam Driscoll <adam.r.driscoll@gmail.com>
2020-11-13 22:34:32 +0000

    New image

--- webcam.ps1
+++ webcam.ps1
@@ -1,6 +1,6 @@
 Set-Location $PSScriptRoot
 Move-Item ./squatch.jpg "./images/$((Get-Date).ToString('s')).jpg"
-fswebcam -r 1280x720 /home/pi/squatch/squatch.jpg
+fswebcam -r 1280x720 ./squatch.jpg
 git pull
 git add .
 git commit -m "New image"
--- webcam.sh
+++
@@ -1,8 +0,0 @@
-!/bin/bash
-
-mv squatch.jpg ./images/$(date +%F-%H:%M).jpg
-fswebcam -r 1280x720 /home/pi/squatch/squatch.jpg
-git pull
-git add .
-git commit -m "New image"
-git push
